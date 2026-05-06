d10cc8e326fdc4fe2bea187a106de46576171803
Cherith <114171444+Cherithh@users.noreply.github.com>
2026-05-06 20:13:21 +0530

    Day 3 Learning JSON

---
+++ Utils/data_reader.py
@@ -0,0 +1,7 @@
+import json
+
+def get_test_data():
+
+    with open("test_data.json",'r') as f:
+        data = json.load(f)
+    return data
--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,14 @@
 from pages.login_page import Loginpage
+from Utils.data_reader import get_test_data
 
 def test_login():
+    data = get_test_data()
     login = Loginpage()
-    login.enter_username("Admin123")
-    login.enter_password("password")
+    
+    username = data["Valid_user"]["username"]
+    password = data["valid user"]["password"]
+    login.enter_username(username)
+    login.enter_password(password)
     login.click_login()
 
 test_login()
